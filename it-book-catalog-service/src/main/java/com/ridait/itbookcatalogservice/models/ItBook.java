2f7fc6e377f097ad8af5cb17948787e4c003377b
ridait <rejra2012@gmail.com>
2019-10-08 00:37:25 +0100

    make catalog service call info service via restTemplate

---
+++ it-book-catalog-service/src/main/java/com/ridait/itbookcatalogservice/models/ItBook.java
@@ -0,0 +1,52 @@
+package com.ridait.itbookcatalogservice.models;
+
+public class ItBook {
+
+    private String bookId;
+    private String title;
+    private String author;
+    private String description;
+
+    public ItBook(){
+
+    }
+
+    public ItBook(String bookId, String title, String author, String description) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+    }
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- it-book-catalog-service/src/main/java/com/ridait/itbookcatalogservice/resources/ItBookCatalogResource.java
+++ it-book-catalog-service/src/main/java/com/ridait/itbookcatalogservice/resources/ItBookCatalogResource.java
@@ -1,25 +1,38 @@
 package com.ridait.itbookcatalogservice.resources;
 
+import com.ridait.itbookcatalogservice.models.ItBook;
 import com.ridait.itbookcatalogservice.models.ItBookItem;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class ItBookCatalogResource {
 
+    private RestTemplate restTemplate = new RestTemplate();
+
     @RequestMapping("/{userId}")
     public List<ItBookItem> getCatalog(@PathVariable String userId){
-        return Collections.singletonList(
-                new ItBookItem("Clean Code",
-                        "Uncle Bob",
-                        "clean code reference book",
-                        4,
-                        "Good book")
-        );
+        //let catalog service call info service
+        List<String> bookIds = Arrays.asList("123","456","789");
+
+        return bookIds.stream()
+                .map(bookId -> {
+                    ItBook itBook = restTemplate.getForObject("http://localhost:8082/itbooks/"+bookId,ItBook.class);
+                    return new ItBookItem(itBook.getTitle(),
+                            itBook.getAuthor(),
+                            itBook.getDescription(),
+                            4,
+                            "Good Book");
+                })
+                .collect(Collectors.toList());
+
     }
 }
